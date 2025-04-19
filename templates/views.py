06b67769175d4eaa830cf8d1a2ffb36b3d16f13e
Disha Parmar <parmardisha658@gmail.com>
2025-04-20 01:12:34 +0530

    uuuuuuhhhhhhhh

--- templates/views.py
+++ templates/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotAllowed
 
 def generate_table(request):
     if request.method == 'POST':
         row_count = int(request.POST.get('row_count'))
         return render(request, 'shopping_list.html', {'result': row_count})
-    return render(request, 'shopping_list.html')
+    return HttpResponseNotAllowed(['POST'])  # Explicitly show allowed methods
